569a24c9c70c99fdede4521d70a041d3b40870c0
David Papp <david.papp@vlabs.at>
2022-10-11 12:15:13 +0200

    origin demo state

--- lib/theme/generated_kit.dart
+++ lib/theme/generated_kit.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class KitColors {
-static const button = Color(0xff66e84a);
+static const button = Color(0xff3F4C9B);
 }
 
 class fontFamilies {
@@ -9,11 +9,11 @@ static const button = 'Roboto Mono';
 }
 
 class fontSize {
-static const PrimaryButton = 24.0;
+static const PrimaryButton = 16.0;
 }
 
 class letterSpacing {
-static const button = 0/100;
+static const button = 50/100;
 }
 
- const ButtonRadius = 48.0;
+ const ButtonRadius = 0.0;
